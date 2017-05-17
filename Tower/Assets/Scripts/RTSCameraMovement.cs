3296a723bcd896154d9da4b9b220d709d02eed52
Liljan <marcus.t.lilja@gmail.com>
2017-05-17 23:51:16 +0200

    Pan by dragging mouse to edge of corner.

--- Tower/Assets/Scripts/RTSCameraMovement.cs
+++ Tower/Assets/Scripts/RTSCameraMovement.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class RTSCameraMovement : MonoBehaviour
 {
     public float ScrollSpeed = 15.0f;
-    public float ScrollEdge = 0.0f;
+    public float ScrollEdge = 0.03f;
 
     private float HorizontalScroll = 1.0f;
     private float VerticalScroll = 1.0f;
@@ -29,22 +29,23 @@ public class RTSCameraMovement : MonoBehaviour
     {
         float speedFactor = transform.position.y / zoomRange.y;
 
+        float mouseX = Input.mousePosition.x;
+        float mouseY = Input.mousePosition.y;
 
-
-        if (Input.GetButton("Up"))
+        if (Input.GetButton("Up") || mouseY > (1F - ScrollEdge) * Screen.height)
         {
             transform.Translate(transform.forward * Time.deltaTime * speedFactor * PanSpeed, Space.World);
         }
-        else if (Input.GetButton("Down"))
+        else if (Input.GetButton("Down") || mouseY < ScrollEdge * Screen.height)
         {
             transform.Translate(-transform.forward * Time.deltaTime * speedFactor * PanSpeed, Space.World);
         }
 
-        if (Input.GetButton("Right"))
+        if (Input.GetButton("Right") || mouseX > (1F - ScrollEdge) * Screen.width)
         {
             transform.Translate(transform.right * Time.deltaTime * speedFactor * PanSpeed, Space.World);
         }
-        else if (Input.GetButton("Left"))
+        else if (Input.GetButton("Left") || mouseX < ScrollEdge * Screen.width)
         {
             transform.Translate(-transform.right * Time.deltaTime * speedFactor * PanSpeed, Space.World);
         }
